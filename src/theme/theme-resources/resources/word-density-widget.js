8dade1aff9ccbe36e45f4d6685cafe4760027039
Ian Maurmann <ikmxx@yahoo.com>
2023-08-30 01:45:18 -0700

    Add front-end validation for URL

--- src/theme/theme-resources/resources/word-density-widget.js
+++ src/theme/theme-resources/resources/word-density-widget.js
@@ -29,13 +29,53 @@ WordDensityWidget.listen = function(){
 
 // Handle On Click "Add New Url" Button
 WordDensityWidget.handleOnClickAddNewUrlButton = function(element, event){
-    let self = WordDensityWidget;
+    let self    = WordDensityWidget;
+    let new_url = '';
 
     Swal.fire({
         heightAuto: false,
-        html:  'You clicked the Add URL button',
-        icon: 'success'
+        html:  'Type-in / Copy-paste URL',
+        icon: '',
+        input: 'text',
+        inputAttributes: {
+            autocapitalize: 'off'
+        },
+        preConfirm: (given_input) => {
+            let is_url = self.isValidHttpUrl(given_input);
+
+            if(is_url){
+                new_url = given_input;
+                return true;
+            }
+            else{
+                Swal.showValidationMessage('Input must be a URL, with http:// or https://');
+                return false;
+            }
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                html: 'URL',
+                icon: 'success',
+            });
+        }
     });
+}
+
+
+WordDensityWidget.isValidHttpUrl = function(given_url_string){
+    // Based on the answer by Pavlo on question:
+    // https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+
+    let url;
+
+    try {
+        url = new URL(given_url_string);
+    } catch (_) {
+        return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:";
 }
 
 
