c9a77f9da98c85279864eefbc4a4f8fae1087ddc
Ian Maurmann <ikmxx@yahoo.com>
2023-08-30 23:22:40 -0700

    New table gateway object, Density Test Word Gateway

---
+++ src/application/model/tests/DensityTestWordGateway.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Density Test Word Gateway
+ * -------------------------
+ *
+ * @noinspection PhpPropertyNamingConventionInspection      - Long property names are ok.
+ * @noinspection PhpMethodNamingConventionInspection        - Long method names are ok.
+ * @noinspection PhpVariableNamingConventionInspection      - Short variable names are ok.
+ * @noinspection PhpUnnecessaryLocalVariableInspection      - Ignore for readability.
+ * @noinspection PhpArrayShapeAttributeCanBeAddedInspection - Ignore shape for now, add later.
+ * @noinspection PhpIllegalPsrClassPathInspection           - Ignore, using PSR 4 not 0.
+ * @noinspection PhpUnusedLocalVariableInspection           - Readability.
+ */
+
+
+declare(strict_types=1);
+
+
+namespace WordDensityDemo\WordDensityApplication;
+
+
+use Exception;
+use PDO;
+use Pith\Framework\PithDatabaseWrapper;
+use Pith\Framework\PithException;
+
+/**
+ * Class DensityTestWordGateway
+ * @package WordDensityDemo\WordDensityApplication
+ */
+class DensityTestWordGateway
+{
+    private PithDatabaseWrapper $database;
+
+    public function __construct(PithDatabaseWrapper $database)
+    {
+        // Set object dependencies:
+        $this->database = $database;
+    }
+
+}
